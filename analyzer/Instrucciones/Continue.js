a521ff1ef7ec508b19f55fc7e3debe450a1d0880
Diamg12 <diegomartinezgt502@gmail.com>
2021-12-11 19:13:00 -0600

    Continue agregado
    Continue

---
+++ analyzer/Instrucciones/Continue.js
@@ -0,0 +1,18 @@
+
+class Primitivos{
+    fila;
+    columna;
+
+
+    constructor(fila, columna){
+        this.fila = fila;
+        this.columna = columna;
+        
+        
+    }
+
+    ejecutar(ent, arbol){
+       //pass
+    }
+
+}
